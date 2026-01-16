5082e973302687977532cbef191e5b981f0f5b4a
Vladislav <vladislav.pikalov@gmail.com>
2026-01-16 12:36:54 +0100

    fix: improved upload component, added maxFiles validation and disabled state

--- src/runtime/components/upload/index.vue
+++ src/runtime/components/upload/index.vue
@@ -1,48 +1,67 @@
 <script setup lang="ts">
 import { useDropZone, useFileDialog } from "@vueuse/core";
-import { toRefs, useTemplateRef } from "vue";
+import { computed, toRefs, useTemplateRef } from "vue";
 
 interface Props {
-  multiple?: boolean;
+  maxFiles?: number;
   allowedTypes?: string[];
+  disabled?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  multiple: true,
+  maxFiles: undefined,
   allowedTypes: undefined,
+  disabled: false,
 });
 
-const { multiple, allowedTypes } = toRefs(props);
+const { maxFiles, allowedTypes, disabled } = toRefs(props);
+
+const isMultiple = computed(
+  () => maxFiles.value === undefined || maxFiles.value > 1,
+);
 
 const dropZone = useTemplateRef("dropZone");
 
-const modelValue = defineModel<File[] | null>();
+const modelValue = defineModel<File[]>({ default: () => [] });
 
 function onDrop(files: File[] | null) {
+  if (disabled.value) return;
   if (files && files.length > 0) {
-    modelValue.value = files;
+    const finalFiles = [...modelValue.value, ...files].slice(
+      0,
+      maxFiles.value ?? -1,
+    );
+    modelValue.value = finalFiles;
   } else {
-    modelValue.value = null;
+    modelValue.value = [];
   }
 }
 
-const { isOverDropZone } = useDropZone(dropZone, {
+// Since we want to have more control over disabled upload - rename the isOverDropZone to overwrite it as a computed
+const { isOverDropZone: overDropZone } = useDropZone(dropZone, {
   onDrop,
-  multiple: multiple.value,
+  multiple: isMultiple.value,
   dataTypes: allowedTypes.value,
   preventDefaultForUnhandled: false,
 });
 
 const { open, onChange } = useFileDialog({
-  multiple: multiple.value,
+  multiple: isMultiple.value,
   accept: allowedTypes.value?.join(","),
 });
+
+function openDialog() {
+  if (disabled.value) return;
+  open();
+}
+
+const isOverDropZone = computed(() => !disabled.value && overDropZone.value);
 
 onChange((files) => onDrop(files as File[] | null));
 </script>
 
 <template>
   <div ref="dropZone">
-    <slot :is-over-drop-zone :open />
+    <slot :is-over-drop-zone :open-dialog />
   </div>
 </template>
