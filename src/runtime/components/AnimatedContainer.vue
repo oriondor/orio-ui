52998089eb2a007b64b61356de888964a01bad50
Vladislav <vladislav.pikalov@gmail.com>
2026-01-20 13:40:41 +0100

    fix: added animated container

---
+++ src/runtime/components/AnimatedContainer.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+interface Props {
+  direction?: "column" | "row";
+}
+
+withDefaults(defineProps<Props>(), {
+  direction: "row",
+});
+</script>
+
+<template>
+  <div class="animated-container" :class="[direction]">
+    <slot />
+  </div>
+</template>
+
+<style>
+.animated-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+  padding-inline: 1rem;
+}
+
+.animated-container.column {
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.animated-container > * {
+  animation: containerFadeInUp 0.5s ease-out both;
+}
+
+@keyframes containerFadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
